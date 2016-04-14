d7f269324d96a772cd18ee1312f0bc60e0f31820
bgveenstra <b.g.veenstra@gmail.com>
2016-04-14 07:17:41 -0700

    added find

---
+++ myFind.js
@@ -0,0 +1,20 @@
+// See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Find
+function myFind(arr, callback) {
+
+//  CODE INSIDE HERE   //
+
+}
+
+/*
+ Best if you don't code out here.
+ If you want to check your code, use `index.js`!
+*/
+
+
+
+
+
+
+
+// export this function (you can ignore this for now)
+module.exports = myFind;
