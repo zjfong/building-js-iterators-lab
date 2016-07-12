4879cede8c00af3492714bbf24f69bfca861ef26
nathanallen <thenathanator@gmail.com>
2016-07-12 16:37:12 -0700

    solution myMap

--- myMap.js
+++ myMap.js
@@ -1,13 +1,16 @@
 /* write your myMap function body in the function below */
 
 function myMap(arr, callback) {
+  var output = [],
+      len = arr.length-1;
 
-  //////////////////////////////////
-  //  YOUR CODE HERE
-  //////////////////////////////////
+  for(var i=0; i<=len; i++){
+    output.push(callback(arr[i], i, arr));
+  }
 
-
+  return output;
 }
+
 
 
 
