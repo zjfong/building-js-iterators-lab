7f46e820a25e3379178178e45a53bd1267ade3ec
nathanallen <thenathanator@gmail.com>
2016-07-12 16:37:22 -0700

    solution myReduce

--- myReduce.js
+++ myReduce.js
@@ -1,12 +1,18 @@
 /* write your myReduce function body in the function below */
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
+  if (arr.length === 0) {
+    return initialValue;
+  }
 
-  //////////////////////////////////
-  //  YOUR CODE HERE
-  //////////////////////////////////
+  var previous_value = initialValue ? callback(initialValue, arr[0], 0, arr) : arr[0];
 
+  var len = arr.length-1;
+  for(var i=1; i<=len; i++){
+    previous_value = callback(previous_value, arr[i], i, arr)
+  }
 
+  return previous_value;
 }
 
 
